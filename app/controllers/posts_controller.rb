0af1b785a12376fab61c615c318c507c1d7e992f
Duy Huynh <dhuynh88@uw.edu>
2016-06-02 19:27:51 -0700

    Added all views. Converted to haml. Added user session links and appropriate paths

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @posts = Post.all.order('created_at DESC')
   end
@@ -18,7 +20,6 @@ class PostsController < ApplicationController
       render 'new'
     end
 
-    redirect_to @post
   end
 
   def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
 
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: {minimum: 5}
-  validates :body, prescence: true
+  validates :body, presence: true
 end
